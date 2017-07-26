9d1e8cdea1223b37060f7d945b22451dca547c10
Lion Ralfs <lion.ralfs@gmx.de>
2017-07-26 18:14:45 +0200

    Started working on API Integration

--- index.js
+++ index.js
@@ -1,65 +1,28 @@
 #!/usr/bin/env node
 'use strict';
 
-const fs = require('fs');
-const co = require('co');
-const prompt = require('co-prompt');
 const program = require('commander');
-const chalk = require('chalk');
 const {version} = require('./package.json');
-const keyFile = 'key.json';
 
-const init = () => {
-    co(function* () {
-        const apiKey = yield prompt.password('Please enter your API-Key. ' +
-            'You can find it at https://mitey.mite.yo.lk/myself\n');
-        fs.open(keyFile, 'r', (err, data) => {
-            if (err) {
-                // file doesn't exist
-                saveKey(apiKey);
-            } else {
-                fs.readFile(keyFile, (err) => {
-                    if (!err) {
-                        co(function* () {
-                            const override = yield prompt.confirm('It seems like you have already registered an API-Key.\n' +
-                                'Do you want to override it? (y/n)\n');
-                            if (override) {
-                                saveKey(apiKey);
-                            }
-                            process.stdin.pause();
-                        });
-                    }
-                });
-            }
-        });
-    });
-};
+const cmds = require('./lib/cmds');
 
-const saveKey = key => {
-    fs.writeFile(keyFile, key, err => {
-        if (err) {
-            return console.log(chalk.red(err));
-        }
-        console.log(chalk.green('Your API-Key was saved!\n'));
-    });
-};
-
-const start = taskname => {
-    if (!taskname) {
-        return console.log(chalk.yellow('Please specify a task name!\n'));
-    }
-    console.log(taskname);
-};
-
+// init
 program
     .command('init')
     .description('Set an API-Key')
-    .action(init);
+    .action(cmds.init);
 
+// start
 program
     .command('start [taskname]')
     .description('Start a task')
-    .action(start);
+    .action(cmds.start);
+
+// verify
+program
+    .command('verify')
+    .description('Verify if mitey has been set up correctly')
+    .action(cmds.verify);
 
 program.version(version);
 program.parse(process.argv);
