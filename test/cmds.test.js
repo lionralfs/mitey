ac6d70a500968e576e9ffb95cd8383baa2a4398f
Lion Ralfs <lion.ralfs@gmx.de>
2017-08-05 19:16:47 +0200

    Added some tests

---
+++ test/cmds.test.js
@@ -0,0 +1,10 @@
+const {expect} = require('chai');
+const cmds = require('../lib/cmds');
+
+describe('Commands', () => {
+    describe('verify', () => {
+        it('should render a text', () => {
+            // TODO
+        })
+    });
+});
